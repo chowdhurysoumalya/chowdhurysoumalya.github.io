454f15d174ef4678a9d37551987c07bda2778d7b
SOUMALYA CHOWDHURY <soumalya9862@gmail.com>
2022-07-24 22:10:33 +0530

    Updating Resume

--- public/export.js
+++ public/export.js
@@ -1,13 +1,11 @@
 window.onload = function () {
     document.getElementById("export")
         .addEventListener("click", () => {
-            const invoice = this.document.getElementById("resume");
-            console.log(invoice);
-            console.log(window);
+            const invoice = this.document.getElementById("resume");           
             var opt = {
                
                 filename: 'Soumalya_Chowdhury.pdf',
-                html2canvas: { scale: 1 },
+                html2canvas: { scale: 1.5 },
                 jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
